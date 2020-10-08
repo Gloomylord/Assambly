0915faa24b22f3fe5a7ee851beaf1844df492f08
Dimitry Ivanov <saaboteur2012@yandex.ru>
2020-10-08 17:34:43 +0300

    make creative card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from "./components/Card";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Card url={'/laser.png'} title={'Лазер'} imgPos={'left center'}/>
+            <Card url={'/img1.jpg'} title={'волк'}/>
+            <Card url={'/animations.png'} title={'анимации'}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Card/index.js
@@ -0,0 +1,34 @@
+import React, {useMemo} from 'react';
+import styles from './style.module.css';
+import cn from 'classnames';
+import Tilt from 'react-tilt';
+
+const gradArr = [ '#f04fa1, #ff5c5f','#f8e189, #f18481','#ea31c2, #6390e9','#55c685, #255286'];
+
+function Card({url, title,imgPos = 'center'}) {
+    let gradMemo = useMemo(()=>{
+        return `linear-gradient(-5deg,${gradArr[Math.round(Math.random() * gradArr.length - .5)]})`;
+    },[]);
+    return <Tilt className={cn(styles.main)} options={{
+        max: 25,
+        perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
+        scale: 1.1,      // 2 = 200%, 1.5 = 150%, etc..
+        speed: 1000,    // Speed of the enter/exit transition
+        reset: true,    // If the tilt effect has to be reset on exit.
+        easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+    }}>
+        <div className={styles.img}
+             style={{
+                 '--url': `url(${url})`,
+                 '--pos': imgPos,
+             }}>
+            <section className={styles.titleContainer}>
+                <div style={{background: gradMemo}}/>
+                <div style={{background: gradMemo}}/>
+                <h3 className={styles.title}>{title}</h3>
+            </section>
+        </div>
+    </Tilt>
+}
+
+export default Card;
